4b3f26a07b73c2e2a1643c00d5ed2fba434f4d5c
Maurocoradoca <mauricioadriancoradoca@gmail.com>
2026-03-02 11:45:42 -0600

    Finalizacion del porgrama, funcion generar porceso, y calcular promedio final

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import simpy 
 import random 
+import statistics
 
 env = simpy.Environment()
 ram = simpy.Container(env, init=100, capacity=100)
@@ -27,3 +28,17 @@ def proceso(env, nombre, ram_sistema, cpu_sistema, instrucciones_totales, memori
     tiempo_total = env.now - llegada
     lista_tiempos.append(tiempo_total) 
     print(f"T={env.now}: {nombre} termina (estuvo {tiempo_total} unidades)")
+
+def generar_procesos(env, ram_sistema, cpu_sistema):
+    for i in range(10):
+        instrucciones = random.randint(1, 10)
+        memoria = random.randint(1, 10)
+        env.process(proceso(env, f"Proceso-{i+1}", ram_sistema, cpu_sistema, instrucciones, memoria))
+        yield env.timeout(random.expovariate(1.0 / tiempoDeLlegada))
+
+env.process(generar_procesos(env, ram, cpu))
+env.run()
+
+if len(lista_tiempos) > 0:
+    promedio = statistics.mean(lista_tiempos)
+    print(f"\nPromedio final de: {promedio}")
